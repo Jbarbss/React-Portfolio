8ce6e72cbfc9b6dae963c8bb1200e71361937248
Jbarbss <jbarbanel@gmail.com>
2021-06-29 14:24:06 -0400

    finnished navbarcss and js files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,90 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { MdFingerprint } from 'react-icons/md'
-import { FaBars , FaTimes } from 'react-icons/fa'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 function Navbar() {
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
-    return (
-        <div className="navbar">
-            <div className="navbar-container continer">
-            <Link to="/" className='navbar-logo'>
-            Jason Barbanel
+  const [click, setClick] = useState(false);
+  
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  
+
+ 
+
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <nav className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              
+              Jason Barbanel
             </Link>
-            <div className="menu-icon" onClick={handleClick} />
-            {click ? <FaTimes/> : <FaBars/>}
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
             </div>
-        </div>
-    )
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/features'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Features
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/about-me'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  About Me
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/portfolio'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Portfolio
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/resume'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Resume
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/contact'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Contact
+                </Link>
+              </li>
+              </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
